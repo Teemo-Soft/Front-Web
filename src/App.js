c7332c487b273ad23526959082436cbb44e461d7
ingguerra9 <ingguerra9@gmail.com>
2019-01-12 17:02:13 -0500

    Login

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import Login from './views/Login'
+
+class App extends Component {
+  render() {
+    return (
+      <Login></Login>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+
+
+class Login extends Component {
+  render() {
+    return (
+      <div className='login-form'>
+        <style>{`
+        body > div,
+        body > div > div,
+        body > div > div > div.login-form {
+          height: 100%;
+        }
+      `}</style>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='teal' textAlign='center'>
+              <Image src='/logo.png' /> Log-in to your account
+          </Header>
+            <Form size='large'>
+              <Segment stacked>
+                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail o Usuario' />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Contraseña'
+                  type='password'
+                />
+                <Button color='teal' fluid size='large'>
+                  Login
+              </Button>
+              </Segment>
+            </Form>
+            <Message>
+              ¿No eres Usuario? Registrate Ahora---<a href='#'>Registrarse</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Login;
