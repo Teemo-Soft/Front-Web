10c5943650b0c38ae9ed101da3cf3c78aa4c0e79
ingguerra9 <ingguerra9@gmail.com>
2019-01-26 16:43:09 -0500

    Integración de apolo client

--- src/index.js
+++ src/index.js
@@ -4,8 +4,24 @@ import './index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css'
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  headers:{
+    Application: '123'
+  }
+});
+
+
+const _App = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+ReactDOM.render(<_App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/views/Login.js
+++ src/views/Login.js
@@ -4,9 +4,41 @@ import { GoogleLogin } from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
 import logo from '../assets/img/logo.png';
 import './Login.css'
-
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      password: ''
+    };
+
+    this.handleChangeUser = this.handleChangeUser.bind(this);
+    this.handleChangePass = this.handleChangePass.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChangeUser(event) {
+    this.setState({ user: event.target.value });
+  }
+
+  handleChangePass(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const { password, user } = this.state
+    console.log(user);
+    console.log(password);
+    event.preventDefault();
+  }
+
+
+
   responseGoogle = (response) => {
     console.log(response);
   }
@@ -30,15 +62,23 @@ class Login extends Component {
             <Header as='h2' color='teal' textAlign='center'>
               <Image src={logo} /> Inicia sesión con tu cuenta
           </Header>
-            <Form size='large'>
+            <Form size='large' onSubmit={this.handleSubmit}>
               <Segment stacked>
-                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail o Usuario' />
+                <Form.Input
+                  fluid icon='user'
+                  iconPosition='left'
+                  placeholder='E-mail o Usuario'
+                  value={this.state.user}
+                  onChange={this.handleChangeUser}
+                />
                 <Form.Input
                   fluid
                   icon='lock'
                   iconPosition='left'
                   placeholder='Contraseña'
                   type='password'
+                  value={this.state.password}
+                  onChange={this.handleChangePass}
                 />
                 <Grid columns='3' padded>
                   <Grid.Column className="Without-padding">
@@ -67,7 +107,7 @@ class Login extends Component {
                       callback={this.responseFacebook}
                       size='small'
                       // cssClass="my-facebook-button-class"
-                      buttonStyle={{ borderRadius: 5, width: 123, height:42, paddingRight: 7 , backgroundColor:'#4C69BA', color:'white' }}
+                      buttonStyle={{ borderRadius: 5, width: 123, height: 42, paddingRight: 7, backgroundColor: '#4C69BA', color: 'white' }}
                     />
                   </Grid.Column>
                 </Grid>
